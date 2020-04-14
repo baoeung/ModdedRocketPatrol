97d5ecba06153d5b1e63e806d1bbbb3c9731c07b
Brandon <bleung7@ucsc.edu>
2020-04-13 18:12:34 -0700

    Test Push

---
+++ src/main.js
@@ -0,0 +1,19 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ],
+};
+
+let game = new Phaser.Game(config);
+
+//reserve some keyboard variable
+let keyF, keyLEFT, keyRIGHT;
+
+// define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000    
+}
+
+//Test Push
